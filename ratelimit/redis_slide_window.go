037d6a320de148e80f2763b9e9e11c59d75aad67
qinye <1426325504@qq.com>
2024-06-03 15:28:33 +0800

    限流器插件，使用redis lua 实现滑动窗口

---
+++ ratelimit/redis_slide_window.go
@@ -0,0 +1,34 @@
+package ratelimit
+
+import (
+	"context"
+	_ "embed"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+//go:embed slide_window.lua
+var luaSlideWindow string
+
+type RedisSlideWindowLimiter struct {
+	cmd redis.Cmdable
+	// 窗口大小
+	interval time.Duration
+	// 阈值
+	rate int
+}
+
+// NewRedisSlideWindowLimiter
+// 新增redis 滑动窗口
+func NewRedisSlideWindowLimiter(cmd redis.Cmdable, interval time.Duration, rate int) Limiter {
+	return &RedisSlideWindowLimiter{
+		cmd:      cmd,
+		interval: interval,
+		rate:     rate,
+	}
+}
+
+func (r *RedisSlideWindowLimiter) Limit(ctx context.Context, key string) (bool, error) {
+	return r.cmd.Eval(ctx, luaSlideWindow, []string{key},
+		r.interval.Milliseconds(), r.rate, time.Now().UnixMilli()).Bool()
+}
