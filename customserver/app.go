578c468095e1162b66dd1536e4fcf85841bb52b8
qinye <1426325504@qq.com>
2024-07-06 09:07:46 +0800

    补充 customserver

--- customserver/app.go
+++ customserver/app.go
@@ -1,4 +1,15 @@
 package customserver
 
+import (
+	"github.com/dadaxiaoxiao/go-pkg/ginx"
+	"github.com/dadaxiaoxiao/go-pkg/grpcx"
+	"github.com/dadaxiaoxiao/go-pkg/saramax"
+	"github.com/robfig/cron/v3"
+)
+
 type App struct {
+	GRPCServer *grpcx.Server
+	GinServer  *ginx.Server
+	Consumers  []saramax.Consumer
+	Crons      []*cron.Cron
 }
